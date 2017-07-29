b47d8f09bf3bf47856694119b27627ed555d0513
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-07-29 11:33:45 +0200

    Implement digest and slugs

--- mikulov/backend.py
+++ mikulov/backend.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
 import sys
+import hashlib
+import re
 
 
 logger = logging.getLogger(__name__)
@@ -16,9 +18,63 @@ root.addHandler(ch)
 logger = logging.getLogger('backend')
 
 
+async def post_digest(title, text):
+    m = hashlib.sha256()
+    m.update(title)
+    m.update(text)
+    return m.hexdigest()[:8]
+
+
 async def make_a_post(data):
     logger.info("make_a_post")
+    title = data['title']
+    text = data['text']
+
+    slug = await slugify(title)
+    digest = await post_digest(title, text)
+    # TODO: throw exception if the folder already exists
+    url_part = "{0}-{1}".format(digest, slug)
+
     return (
         "wubbadubbadubdubs",
-        "1234-test-post"
+        url_part
     )
+
+
+async def slugify(s):
+    """
+    http://blog.dolphm.com/slugify-a-string-in-python/
+    Simplifies ugly strings into something URL-friendly.
+    >>> print slugify("[Some] _ Article's Title--")
+    some-articles-title
+    """
+
+    # "[Some] _ Article's Title--"
+    # "[some] _ article's title--"
+    s = s.lower()
+
+    # "[some] _ article's_title--"
+    # "[some]___article's_title__"
+    for c in [' ', '-', '.', '/']:
+        s = s.replace(c, '_')
+
+    # "[some]___article's_title__"
+    # "some___articles_title__"
+    s = re.sub('\W', '', s)
+
+    # "some___articles_title__"
+    # "some   articles title  "
+    s = s.replace('_', ' ')
+
+    # "some   articles title  "
+    # "some articles title "
+    s = re.sub('\s+', ' ', s)
+
+    # "some articles title "
+    # "some articles title"
+    s = s.strip()
+
+    # "some articles title"
+    # "some-articles-title"
+    s = s.replace(' ', '-')
+    return s
